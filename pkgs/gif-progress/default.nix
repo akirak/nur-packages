936371e7737b3a2a08defcfa30843ad41e4e463d
Akira Komamura <akira.komamura@gmail.com>
2020-12-10 04:07:10 +0900

    Fix hash mismatches

--- pkgs/gif-progress/default.nix
+++ pkgs/gif-progress/default.nix
@@ -1,28 +1,27 @@
 { runCommandNoCC
 }:
 runCommandNoCC "gif-progress-release"
-  {
-    version = "0";
+{
+  version = "0";
 
-    src =
-      if builtins.currentSystem == "x86_64-linux"
-      then
-        builtins.fetchTarball
-          {
-            url = "https://github.com/nwtgck/gif-progress/releases/download/release-fix-not-moving-progress-bar/gif-progress-linux-amd64.tar.gz";
-            sha256 = "139zm9bbmnayn52myfjrshmg5wagzvghqxagv4g1b2dznjrxd4vn";
-            # date = 2019-12-31T04:50:01+0900;
-          }
-      else
-        throw "Unsupported system";
+  src =
+    if builtins.currentSystem == "x86_64-linux"
+    then
+      builtins.fetchTarball
+        {
+          url = "https://github.com/nwtgck/gif-progress/releases/download/release-fix-not-moving-progress-bar/gif-progress-linux-amd64.tar.gz";
+          sha256 = "1glxpcyih5afb0zzq826yxhrayf91z82x3fk02iwsl5jf0kgy4dv";
+        }
+    else
+      throw "Unsupported system";
 
-    archive =
-      if builtins.currentSystem == "x86_64-linux"
-      then
-        "gif-progress-linux-amd64"
-      else
-        throw "Unsupported system";
-  }
+  archive =
+    if builtins.currentSystem == "x86_64-linux"
+    then
+      "gif-progress-linux-amd64"
+    else
+      throw "Unsupported system";
+}
   ''
     mkdir -p $out/bin
     install -t $out/bin $src/$archive/gif-progress
--- pkgs/gitbatch/default.nix
+++ pkgs/gitbatch/default.nix
@@ -1,19 +1,19 @@
 { runCommandNoCC
 }:
 runCommandNoCC "gitbatch-0.5.0"
-  rec {
-    version = "0.5.0";
-    src =
-      if builtins.currentSystem == "x86_64-linux"
-      then
-        builtins.fetchTarball
-          {
-            url = "https://github.com/isacikgoz/gitbatch/releases/download/v${version}/gitbatch_${version}_linux_amd64.tar.gz";
-            sha256 = "0dw34m7qlz8i64jmng8ia9mdrr7ggq92ppcq5il3xk18n5qamgyq";
-          }
-      else
-        throw "Unsupported system";
-  }
+rec {
+  version = "0.5.0";
+  src =
+    if builtins.currentSystem == "x86_64-linux"
+    then
+      builtins.fetchTarball
+        {
+          url = "https://github.com/isacikgoz/gitbatch/releases/download/v${version}/gitbatch_${version}_linux_amd64.tar.gz";
+          sha256 = "0sjjj9z1dhilhpc8pq4154czrb79z9cm044jvn75kxcjv6v5l2m5";
+        }
+    else
+      throw "Unsupported system";
+}
   ''
     mkdir -p $out/bin
     install -t $out/bin $src/gitbatch
