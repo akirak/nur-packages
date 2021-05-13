de27909d378e92c580a8c8e298958e876cc0a802
Akira Komamura <akira.komamura@gmail.com>
2021-05-14 02:51:39 +0900

    Update readability-cli to 2.3

--- pkgs/readability-cli/default.nix
+++ pkgs/readability-cli/default.nix
@@ -1,18 +1,26 @@
-{ nodejs,  stdenv, python2, utillinux, runCommand, writeTextFile, fetchurl, fetchgit,
-  runCommandNoCC
+{ nodejs
+, lib
+, stdenv
+, python2
+, utillinux
+, runCommand
+, writeTextFile
+, fetchurl
+, fetchgit
+, runCommandNoCC
 }:
 let
   composition = (import ./composition.nix {
     pkgs = {
-      inherit stdenv python2 utillinux runCommand writeTextFile fetchurl fetchgit;
+      inherit lib stdenv python2 utillinux runCommand writeTextFile fetchurl fetchgit;
     };
     inherit nodejs;
   }).readability-cli;
 in
 runCommandNoCC "readability-cli"
-  {
-    propagateBuildInputs = [ composition ];
-  } ''
-    mkdir -p $out/bin
-    ln -s ${composition}/bin/readable $out/bin/readable
-  ''
+{
+  propagateBuildInputs = [ composition ];
+} ''
+  mkdir -p $out/bin
+  ln -s ${composition}/bin/readable $out/bin/readable
+''
