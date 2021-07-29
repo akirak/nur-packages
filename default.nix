5d46c1f4ed3dee75f28a42408df39f25d8b86974
Akira Komamura <akira.komamura@gmail.com>
2021-07-30 00:25:19 +0900

    Drop derivation that depend on outdated Erlang R20

--- default.nix
+++ default.nix
@@ -81,10 +81,6 @@ in
     inherit (pkgs.stdenv) mkDerivation;
   };
 
-  elixir_1_6_erlangR20 = pkgs.callPackage ./pkgs/elixir/elixir_1_6 {
-    erlang = pkgs.erlangR20;
-  };
-
   elixir_1_6_erlangR21 = pkgs.callPackage ./pkgs/elixir/elixir_1_6 {
     erlang = pkgs.erlangR21;
   };
